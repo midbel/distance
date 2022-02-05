489d3ba29b6e9db87daa68f86d170ad633ad2707
midbel <brun.nicolas21@gmail.com>
2022-02-05 17:07:36 +0100

    add go.mod

--- distance_test.go
+++ distance_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/midbel/maestro/distance"
+	"github.com/midbel/distance"
 )
 
 func TestGetLevenshteinDistance(t *testing.T) {
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/midbel/distance
+
+go 1.18
